26983fe28f5a3558bd881772318810b2d1d0df18
ryanmarinhodev <ryanmarinhodev@gmail.com>
2026-07-18 15:03:18 -0300

    feat: add new items and routes

--- apps/api/start/routes.ts
+++ apps/api/start/routes.ts
@@ -1,4 +1,4 @@
-import Route from '@ioc:Adonis/Core/Route'
+import Route from '@ioc:Adonis/Core/Route';
 
 Route.get('/', async () => {
   return {
@@ -8,9 +8,15 @@ Route.get('/', async () => {
 })
 
 Route.get('/health', async () => {
+  const date = new Date().toISOString()
+  const upTime = Math.floor(process.uptime())
+
   return {
     status: 'ok',
-    timestamp: new Date().toISOString(),
+    appName: process.env.APP_NAME,
     environment: process.env.NODE_ENV ?? 'development',
+    date: date,
+    upTime: upTime,
+
   }
 })
